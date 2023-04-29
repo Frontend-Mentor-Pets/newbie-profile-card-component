666ae91bc16d0d76729d917f0af57bb4a8f6aaca
Bumble <bumble.sakh@gmail.com>
2023-04-30 03:09:57 +1100

    feat: add desktop styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,37 @@
 import styles from './Card.module.scss';
+import avatar from '../../assets/images/image-victor.jpg';
 
 const Card = () => {
-  return <div className={styles.card}></div>;
+  return (
+    <div className={styles.card}>
+      <div className={styles.top}></div>
+      <div className={styles.content}>
+        <div className={styles.avatar}>
+          <img src={avatar} alt='avatar' />
+        </div>
+        <div className={styles.header}>
+          <h1 className={styles.name}>Victor Crest</h1>
+          <span className={styles.age}>26</span>
+        </div>
+        <span className={styles.city}>London</span>
+        <div className={styles.line}></div>
+        <div className={styles.info}>
+          <div className={styles.block}>
+            <p className={styles.title}>80K</p>
+            <p className={styles.subtitle}>Followers</p>
+          </div>
+          <div className={styles.block}>
+            <p className={styles.title}>803K</p>
+            <p className={styles.subtitle}>Likes</p>
+          </div>
+          <div className={styles.block}>
+            <p className={styles.title}>1.4K</p>
+            <p className={styles.subtitle}>Photos</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
